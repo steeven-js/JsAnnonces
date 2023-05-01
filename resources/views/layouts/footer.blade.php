ccfbe396b5bc795ec2fea1c0c19c01ad9e56c247
steeven-js <jacques.steeven@gmail.com>
2023-05-01 11:18:04 -0400

    footer ajustements

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+<footer class="bg-white shadow dark:bg-gray-900">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://jsprod.fr" class="flex items-center mb-4 sm:mb-0">
