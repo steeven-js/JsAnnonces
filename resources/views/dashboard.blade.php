fa648270c0ae8dd1001efc50b5f3faf31d6ffdec
steeven-js <jacques.steeven@gmail.com>
2023-05-01 11:24:22 -0400

    Dasboard Dark/light theme

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,38 +25,55 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 
     <script>
-        const sidebar = document.getElementById('sidebar');
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
 
-        if (sidebar) {
-            const toggleSidebarMobile = (sidebar, sidebarBackdrop, toggleSidebarMobileHamburger,
-                toggleSidebarMobileClose) => {
-                    sidebar.classList.toggle('hidden');
-                    sidebarBackdrop.classList.toggle('hidden');
-                    toggleSidebarMobileHamburger.classList.toggle('hidden');
-                    toggleSidebarMobileClose.classList.toggle('hidden');
+        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+        // Change the icons inside the button based on previous settings
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            themeToggleLightIcon.classList.remove('hidden');
+        } else {
+            themeToggleDarkIcon.classList.remove('hidden');
+        }
+
+        var themeToggleBtn = document.getElementById('theme-toggle');
+
+        themeToggleBtn.addEventListener('click', function() {
+
+            // toggle icons inside button
+            themeToggleDarkIcon.classList.toggle('hidden');
+            themeToggleLightIcon.classList.toggle('hidden');
+
+            // if set via local storage previously
+            if (localStorage.getItem('color-theme')) {
+                if (localStorage.getItem('color-theme') === 'light') {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                } else {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
                 }
 
-            const toggleSidebarMobileEl = document.getElementById('toggleSidebarMobile');
-            const sidebarBackdrop = document.getElementById('sidebarBackdrop');
-            const toggleSidebarMobileHamburger = document.getElementById('toggleSidebarMobileHamburger');
-            const toggleSidebarMobileClose = document.getElementById('toggleSidebarMobileClose');
-            const toggleSidebarMobileSearch = document.getElementById('toggleSidebarMobileSearch');
+                // if NOT set via local storage previously
+            } else {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                } else {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                }
+            }
 
-            toggleSidebarMobileSearch.addEventListener('click', () => {
-                toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger,
-                    toggleSidebarMobileClose);
-            });
-
-            toggleSidebarMobileEl.addEventListener('click', () => {
-                toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger,
-                    toggleSidebarMobileClose);
-            });
-
-            sidebarBackdrop.addEventListener('click', () => {
-                toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger,
-                    toggleSidebarMobileClose);
-            });
-        }
+        });
     </script>
 </body>
 
--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -10,8 +10,8 @@
                             d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    <svg id="toggleSidebarMobileClose" class="hidden w-6 h-6" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <svg id="toggleSidebarMobileClose" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                             clip-rule="evenodd"></path>
@@ -124,7 +124,8 @@
                                 <div class="text-gray-500 font-normal text-sm mb-1.5 dark:text-gray-400"><span
                                         class="font-semibold text-gray-900 dark:text-white">Jese leos</span> and
                                     <span class="font-medium text-gray-900 dark:text-white">5 others</span> started
-                                    following you.</div>
+                                    following you.
+                                </div>
                                 <div class="text-xs font-medium text-primary-700 dark:text-primary-400">10 minutes
                                     ago</div>
                             </div>
@@ -224,8 +225,7 @@
                     class="hidden p-2 text-gray-500 rounded-lg sm:flex hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
                     <span class="sr-only">View notifications</span>
 
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
                         </path>
@@ -335,19 +335,22 @@
                         </a>
                     </div>
                 </div>
-                <button id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button"
+
+                <!-- Dark/light theme -->
+                <button id="theme-toggle" type="button"
                     class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-                    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
                     </svg>
-                    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
                             fill-rule="evenodd" clip-rule="evenodd"></path>
                     </svg>
                 </button>
+                
                 <div id="tooltip-toggle" role="tooltip"
                     class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
                     Toggle dark mode
@@ -361,8 +364,7 @@
                             id="user-menu-button-2" aria-expanded="false" data-dropdown-toggle="dropdown-2">
                             <span class="sr-only">Open user menu</span>
                             <img class="w-8 h-8 rounded-full"
-                                src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                                alt="user photo">
+                                src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
                         </button>
                     </div>
 
@@ -372,8 +374,7 @@
                             <p class="text-sm text-gray-900 dark:text-white" role="none">
                                 Neil Sims
                             </p>
-                            <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
-                                role="none">
+                            <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
                                 neil.sims@flowbite.com
                             </p>
                         </div>
